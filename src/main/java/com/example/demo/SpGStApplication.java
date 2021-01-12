dbf7bd1ff255c946470a0943de483708624c62a4
yuukihongou <64779395+yuukihongou@users.noreply.github.com>
2021-01-12 22:09:37 +0900

    first

---
+++ src/main/java/com/example/demo/SpGStApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpGStApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpGStApplication.class, args);
+	}
+
+}
