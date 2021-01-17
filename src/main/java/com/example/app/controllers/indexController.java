741a610a299522293b52db32758b39bd3e95f103
yuukihongou <64779395+yuukihongou@users.noreply.github.com>
2021-01-17 22:02:58 +0900

    修正

--- src/main/java/com/example/app/controllers/indexController.java
+++ src/main/java/com/example/app/controllers/indexController.java
@@ -1,20 +1,28 @@
 package com.example.app.controllers;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.app.controllers.forms.IndexForm;
 
 @SpringBootApplication
+@ Controller
 public class indexController {
 
-	@RequestMapping(value = "/sp-g-st" )
+    public static void main(String[] args) {
+        SpringApplication.run(indexController.class, args);
+    }
+
+	@RequestMapping(value = "/" )
 	public String init(Model model) {
 
 		model.addAttribute("test", new IndexForm());
 
     	return  "/index.html";
 	}
+
 
 }
